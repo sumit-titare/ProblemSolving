4fbbd827b3ae533b2308ea4b65b940788a046da7
Sumit Titare <sumittitare06@gmail.com>
2025-09-12 11:53:05 +0530

    main_incubyte_test: ITR2 - Added tests to handle any amount of numbers

--- coding_tests/Incubyte/spec/lib/string_calculator_spec.rb
+++ coding_tests/Incubyte/spec/lib/string_calculator_spec.rb
@@ -14,5 +14,21 @@ RSpec.describe StringCalculator do
         expect(subject).to eq(0)
       end
     end
+
+    context 'when numbers contain a single number' do
+      let(:numbers) { '1' }
+
+      it 'returns the number itself' do
+        expect(subject).to eq 1
+      end
+    end
+
+    context 'when numbers contain more than one number separated by a comma' do
+      let(:numbers) { '1,2,3' }
+
+      it 'returns the sum of all the numbers' do
+        expect(subject).to eq(6)
+      end
+    end
   end
 end
