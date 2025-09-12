8630d8170c90bb5fc54bb196d6f14cba89ae4518
Sumit Titare <sumittitare06@gmail.com>
2025-09-12 12:42:48 +0530

    main_incubyte_test: ITR4 -Added code changes to input with custom delimiter

--- coding_tests/Incubyte/lib/string_calculator.rb
+++ coding_tests/Incubyte/lib/string_calculator.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class StringCalculator
+
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    default_delimiter = /,|\n/
+    if numbers.start_with?('//')
+      delimiter_str, numbers = numbers.split("\n", 2)
+      delimiter = Regexp.new(Regexp.escape(delimiter_str[2]))
+    else
+      delimiter = default_delimiter
+    end
+
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
